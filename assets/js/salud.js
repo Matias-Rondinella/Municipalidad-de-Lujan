812677d49f9ea6b912e32bd567d72720737990a5
Matias <matiasrondinellaphoto@gmail.com>
2022-01-26 16:21:43 -0300

    Empezando de cero en salud.html para hacer un sistema de turnos

--- assets/js/salud.js
+++ assets/js/salud.js
@@ -1,38 +1,6 @@
-class Contacto {
-	constructor(nombre, email, telefono, edad) {
-		this.nombre = nombre
-		this.email = email
-		this.telefono = telefono
-		this.edad = edad
-    }​
-	agendar() {
-		console.log("El contacto " + this.nombre + " ha sido agendado")
-	}​
-	validar() {
-		let valido = true
-		if (this.nombre.length < 2) {
-			valido = false
-		} else if (this.email.length < 2) {
-			valido = false
-		} else if (this.telefono.length < 2) {
-			valido = false
-		} else if (this.mensaje.length < 2) {
-			valido = false
-		}​
-		return valido
-        }
-    }​
-        while (true) {
-	const contacto1 = new Contacto(
-		prompt("Nombre: "),
-		prompt("Email: "),
-		prompt("Telefono: "),
-		prompt("Relacion: ")
-	)
-	let crear = prompt("ingresar otro?").toLowerCase()​
-	if (crear == "si") {
-		continue
-	} else {
-		break
-	}
-}
+
+
+
+
+
+
